9a1477b50d4001f6a833328d28ac0135386ff46e
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 15:26:24 -0400

    Delete terraform/outputs.tf

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_id" {
-  description = "The ID of the created VM"
-  value       = proxmox_vm_qemu.example_vm.id
-}
-
-output "vm_name" {
-  description = "The name of the created VM"
-  value       = proxmox_vm_qemu.example_vm.name
-}
