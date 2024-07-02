ec7e1eea00736f3e8b36f7a782cc2091485604c1
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 13:08:58 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,38 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "<version tag>"
+      version = ">=2.9.0"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url         = var.pm_api_url
-  pm_api_token_id    = var.pm_api_token_id
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
-  tls_insecure       = var.pm_tls_insecure
+  pm_tls_insecure     = var.pm_tls_insecure
+}
+
+resource "proxmox_vm_qemu" "example_vm" {
+  name        = var.vm_name
+  description = var.vm_description
+  target_node = var.vm_target_node
+  pool        = var.vm_pool
+  clone       = var.vm_template
+  cores       = var.vm_cores
+  memory      = var.vm_memory
+  disk {
+    size = var.vm_disk_size
+  }
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+  cloudinit {
+    user_data = file("cloud-init.yaml")
+  }
+}
+
+output "vm_id" {
+  value = proxmox_vm_qemu.example_vm.id
 }
