d962e7228fdc27f9f820264d41db51f1ad069b3e
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 13:01:40 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,47 +2,14 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = ">=2.9.7"
+      version = "<version tag>"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url          = var.pm_api_url
-  pm_api_token_id     = var.pm_api_token_id
+  pm_api_url         = var.pm_api_url
+  pm_api_token_id    = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
-  pm_tls_insecure     = var.pm_tls_insecure
-}
-
-resource "proxmox_vm_qemu" "dc" {
-  name        = var.vm_name
-  desc        = var.vm_description
-  target_node = var.vm_target_node
-  pool        = var.vm_pool
-  clone       = var.vm_template
-  cores       = var.vm_cores
-  sockets     = var.vm_sockets
-  memory      = var.vm_memory
-  scsihw      = var.vm_scsi_hw
-
-  disk {
-    size      = "${var.vm_disk_size}G"
-    storage   = var.vm_disk_storage
-    type      = "virtio"
-    cache     = "writeback"
-    iothread  = true
-    discard   = true
-  }
-
-  network {
-    model   = "virtio"
-    bridge  = "vmbr0"
-    tag     = 256
-  }
-
-  ipconfig0 = var.vm_ip
-
-  sshkeys = <<EOF
-ssh-rsa 9182739187293817293817293871== user@pc
-EOF
+  tls_insecure       = var.pm_tls_insecure
 }
