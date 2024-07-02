146a4c2d3fc15e7931e4ebf9b3213c59b3d221b9
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 13:22:33 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,22 +16,32 @@ provider "proxmox" {
 
 resource "proxmox_vm_qemu" "example_vm" {
   name        = var.vm_name
-  description = var.vm_description
   target_node = var.vm_target_node
   pool        = var.vm_pool
   clone       = var.vm_template
   cores       = var.vm_cores
   memory      = var.vm_memory
+
   disk {
-    size = var.vm_disk_size
+    size    = var.vm_disk_size
+    type    = "scsi"
+    storage = "local-lvm"
   }
+
   network {
     model  = "virtio"
     bridge = "vmbr0"
-    ipconfig0 = "ip=192.168.10.100/24,gw=192.168.10.1"
+    # Static IP configuration
+    ip = "192.168.10.100/24"
   }
+
+  ipconfig0 = "ip=192.168.10.100/24,gw=192.168.10.1"
+
   cicustom = "user=local:snippets/cloud-init.yaml"
-  cloudinit {
-    user_data = file("cloud-init.yaml")
-  }
+  # Custom cloud-init configuration
+  ciuser_data = file("cloud-init.yaml")
+}
+
+output "vm_id" {
+  value = proxmox_vm_qemu.example_vm.id
 }
