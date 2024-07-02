fd40f1e068d1b645af54549d2dc1f0a1e0383245
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2024-07-02 16:30:04 -0400

    Update main.tf

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -73,15 +73,11 @@ resource "proxmox_vm_qemu" "dc_vm" {
   boot       = "order=virtio0"
   ipconfig0  = "ip=192.168.10.100/24,gw=192.168.10.1"
 
-  // Define a disk block with media type cdrom which references the generated cloud-init disk
-  disks {
-    scsi {
-      scsi0 {
-        cd {
-          iso = "${local.iso_storage_pool}:${proxmox_cloud_init_disk.ci.id}"
-        }
-      }
-    }
+  disk {
+    id           = 0
+    type         = "scsi"
+    storage      = local.iso_storage_pool
+    storage_type = "cloudinit"
   }
 }
 
